db9da613c2b7667e569fb073ddc020c29fa18bb2
Okkam_Linux <coksharov.kirill2015@yandex.ru>
2026-01-11 18:55:00 +0300

    Рефакторинг тестов: добавление вспомогтаельной функции assertCorrectMessage для дублируюшегося кода с проверкой got и want

--- hello_test.go
+++ hello_test.go
@@ -6,15 +6,19 @@ func TestHello(t *testing.T) {
 	t.Run("Проверка, что функция возвращает Hello, SOME_NAME", func(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello, Chris"
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("Печать 'Hello World' если строка с именем была пустая", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
-		if got != want {
-			t.Errorf("Возвращает: %q Ожидается: %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+// t testing.TB - интерфейс для вспомогательных функций в тесте или бенчмарке
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("Возвращает: %q Ожидается: %q", got, want)
+	}
 }
