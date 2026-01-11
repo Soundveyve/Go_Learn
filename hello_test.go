f524e61eb173174a4ffb504759a737e59d1c2ad2
Okkam_Linux <coksharov.kirill2015@yandex.ru>
2026-01-11 18:44:09 +0300

    Переход на русский язык в описании тестов. Изменение функции Hello: добавление дефолтного World если name это пустая строка. Написание теста на новую функцию

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,18 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+	t.Run("Проверка, что функция возвращает Hello, SOME_NAME", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("Печать 'Hello World' если строка с именем была пустая", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		if got != want {
+			t.Errorf("Возвращает: %q Ожидается: %q", got, want)
+		}
+	})
 }
