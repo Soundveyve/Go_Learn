f26007acbc3dc1b92fc6452e1a27766328936142
Okkam_Linux <coksharov.kirill2015@yandex.ru>
2026-01-15 16:36:42 +0300

    Переработка функции Repeat() с использованием strings.Builder() для конкатенации. Добавление в описании бенчмарка для функции описания флага -benchmem

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,14 +1,26 @@
 package iteration
 
-func Repeat(symbol string, countRepeat int) (repeatString string) {
+import (
+	"strings"
+)
 
-	if countRepeat == 1 || countRepeat == 0 {
+func Repeat(symbol string, countRepeat int) string {
+
+	if countRepeat == 0 || countRepeat == 1 {
 		return symbol
 	}
 
+	/*
+		Строки в Go неизменяемы, то есть при каждой операции конкатенации, как в функции Repeat,
+		происходит копирование памяти для размещения новой строки.
+		Это влияет на производительность, особенно при интенсивной конкатенации строк.
+		Стандартная библиотека предоставляет тип strings.Builder,
+		который минимизирует копирование данных в памяти.
+		Он реализует метод WriteString, который можно использовать для объединения строк:
+	*/
+	var repeated strings.Builder
 	for i := 0; i < countRepeat; i++ {
-		repeatString += symbol
+		repeated.WriteString(symbol)
 	}
-
-	return repeatString
+	return repeated.String()
 }
