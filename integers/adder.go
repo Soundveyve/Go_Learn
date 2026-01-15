df12ad79dcb2796c02981586c9fdbaee27516c5e
Okkam_Linux <coksharov.kirill2015@yandex.ru>
2026-01-15 15:33:42 +0300

    Добавлена функция и тесты для изучения типа integer. README: добавлена инфорация по Example функциям

---
+++ integers/adder.go
@@ -0,0 +1,6 @@
+package integers
+
+// Функция сложения двух чисел
+func Add(x, y int) int {
+	return x + y
+}
--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,1 +1,25 @@
 package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+	if sum != expected {
+		t.Errorf("expected %d but got %d", expected, sum)
+	}
+}
+
+/*
+Пример использования функции Add.
+Комментарий внутри кода ВЛИЯЕТ на тесты: без этого комментария код только скоплириуется, но не выполнится
+Удалите комментарий // Output: 6, затем запустите go test -v, и вы увидите, что ExampleAdd больше не выполняется.
+*/
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
